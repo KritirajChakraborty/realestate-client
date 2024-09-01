6615b939b8fa1ca00cdff93546e2edff949272b9
KritirajChakraborty <kritiraj.exams2021@gmail.com>
2024-09-01 18:43:18 +0530

    complete image upload functionality

--- src/pages/CreateLisitng.jsx
+++ src/pages/CreateLisitng.jsx
@@ -1,6 +1,76 @@
-import React from "react";
-
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { useState } from "react";
 export default function CreateLisitng() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({ imageURLs: [] });
+  const [uploading, setUploading] = useState(false);
+  const [imageUploadError, setImageUploadError] = useState(false);
+
+  const handleImageSubmit = () => {
+    if (files.length > 0 && files.length + formData.imageURLs.length < 7) {
+      setUploading(true);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+
+      Promise.all(promises)
+        .then((url) => {
+          setFormData({
+            ...formData,
+            imageURLs: formData.imageURLs.concat(url),
+          });
+          setUploading(false);
+          setImageUploadError(false);
+        })
+        .catch((err) => {
+          setImageUploadError(`Image upload failed! max 2mb allowed: ${err}`);
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("You can upload only 6 images!");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getDate() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleDeleteImage = (index) => {
+    setFormData({
+      ...formData,
+      imageURLs: formData.imageURLs.filter((_, i) => i != index),
+    });
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -119,16 +189,44 @@ export default function CreateLisitng() {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
               accept="image/*"
               multiple
             />
-            <button className="p-3 text-white border border-green-700 bg-green-700 rounded uppercase hover:bg-green-900 disabled:opacity-80">
-              Upload
+            <button
+              type="button"
+              onClick={handleImageSubmit}
+              className="p-3 text-white border border-green-700 bg-green-700 rounded uppercase hover:bg-green-900 disabled:opacity-80"
+            >
+              {uploading ? "Uploading..." : "Upload"}
             </button>
           </div>
+          <p className="text-red-700 text-sm">
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageURLs &&
+            formData.imageURLs.map((url, index) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 items-center border"
+              >
+                <img
+                  src={url}
+                  alt="image uploaded"
+                  className="w-20 h-20 object-contain rounded-lg"
+                />
+                <button
+                  onClick={() => handleDeleteImage(index)}
+                  type="button"
+                  className="text-red-500 uppercase hover:text-red-700"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
             Create Listing
           </button>
